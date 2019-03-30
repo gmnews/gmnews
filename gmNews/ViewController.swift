48f053ba5e16195bd54e11948e3a7ca9ee30b1de
Martin Landin <martinlandin@Martins-Air.attlocal.net>
2019-03-29 17:56:55 -0700

    Initial storyboards created

---
+++ gmNews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gmNews
+//
+//  Created by Martin Landin on 3/27/19.
+//  Copyright © 2019 Martin Landin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
